8344805d99412d692597be22f5ded1d52b048cbe
Michael Sun <msun2019@pingry.org>
2016-05-12 13:55:57 -0400

    Program working, added test cases

---
+++ gradeBook.py
@@ -0,0 +1,21 @@
+grades = [[100, 97, 98, 93, 91],
+		 [   0,  0, 50, 79, 60],
+		 [  85, 89, 92, 79, 80]]
+		 
+		 
+def average_rows(grid, targetRow):
+	rowCounter = -1 
+	answer = 0 
+	for row in grid:
+		itemCounter = 0.0
+		rowCounter += 1 
+		if rowCounter == targetRow:
+			for item in row:
+				itemCounter += 1 
+				answer += item
+			
+			return answer / itemCounter 
+
+print("The average of the grades in the first row are: {}".format(average_rows(grades, 0)))
+print("The average of the grades in the first row are: {}".format(average_rows(grades, 1)))
+print("The average of the grades in the first row are: {}".format(average_rows(grades, 2)))
